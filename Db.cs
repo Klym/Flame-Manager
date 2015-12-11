cddc6df5559e3f3d1987df73bfabc8cd6e3d72b1
Maksim Klimenko <klym.programmer@yandex.ru>
2015-12-12 02:02:31 +0300

    fix Db class

--- Db.cs
+++ Db.cs
@@ -11,6 +11,10 @@ namespace Flame_Manager {
     class Db {
         private MySqlConnection connection;
         private Process plink;
+        private string mysqlHost;
+        private string ftpHost;
+        private string database;
+        private string pass;
 
         public MySqlConnection Connection {
             get {
@@ -20,31 +24,43 @@ namespace Flame_Manager {
 
         public Db() {
             // Чтение хостов и паролей из файла
-            FileStream fs = new FileStream(@"pass.txt", FileMode.Open);
-            StreamReader file = new StreamReader(fs);
-            string mysqlHost = file.ReadLine();
-            string ftpHost = file.ReadLine();
-            string database = file.ReadLine();
-            string pass = file.ReadLine();
-            file.Close();
-            fs.Close();
+            this.deserializeSettings();
 
             // Открытие SSH туннеля
+            this.openSSHTunnel();
+
+            // Соединение с базой данных
+            try {
+                this.connect();
+            } catch (Exception) {
+                this.plink.Kill();
+                throw new Exception("Не удается установить соединение с базой.");
+            }
+        }
+
+        private void deserializeSettings() {
+            FileStream fs = new FileStream(@"pass.txt", FileMode.Open);
+            StreamReader file = new StreamReader(fs);
+            this.mysqlHost = file.ReadLine();
+            this.ftpHost = file.ReadLine();
+            this.database = file.ReadLine();
+            this.pass = file.ReadLine();
+            file.Close();
+            fs.Close();
+        }
+
+        private void openSSHTunnel() {
             this.plink = new Process();
             this.plink.StartInfo.FileName = @"plink.exe";
             this.plink.StartInfo.Arguments = "-L localhost:3306:" + mysqlHost + ":3306 -pw " + pass + " " + ftpHost;
             this.plink.StartInfo.CreateNoWindow = true;
             this.plink.StartInfo.UseShellExecute = false;
             this.plink.Start();
+        }
 
-            // Соединение с базой данных
-            try {
-                this.connection = new MySqlConnection("SERVER=127.0.0.1; Port=3306; DATABASE=" + database + "; UID=" + database + "; PASSWORD=" + pass + ";");
-                this.connection.Open();
-            } catch (Exception) {
-                this.plink.Kill();
-                throw new Exception("Не удается установить соединение с базой.");
-            }
+        private void connect() {
+            this.connection = new MySqlConnection("SERVER=127.0.0.1; Port=3306; DATABASE=" + database + "; UID=" + database + "; PASSWORD=" + pass + ";");
+            this.connection.Open();
         }
 
         ~Db() {
